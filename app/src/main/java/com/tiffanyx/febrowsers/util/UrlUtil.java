c44c924fa1b52234545bd31c74e9fc980e438eab
cynd905 <cyndi905@qq.com>
2020-01-06 14:01:07 +0800

    修复url验证问题

--- app/src/main/java/com/tiffanyx/febrowsers/util/UrlUtil.java
+++ app/src/main/java/com/tiffanyx/febrowsers/util/UrlUtil.java
@@ -5,7 +5,17 @@ import java.util.regex.Pattern;
 
 public class UrlUtil {
     public static boolean isUrl(String s){
-        String regex =  "^([hH][tT]{2}[pP]:/*|[hH][tT]{2}[pP][sS]:/*|[fF][tT][pP]:/*)(([A-Za-z0-9-~]+).)+([A-Za-z0-9-~\\/])+(\\?{0,1}(([A-Za-z0-9-~]+\\={0,1})([A-Za-z0-9-~]*)\\&{0,1})*)$";
+        String regex = "^((ftp|http|https|intent)?://)"                      // support scheme
+                + "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" // ftp的user@
+                + "(([0-9]{1,3}\\.){3}[0-9]{1,3}"                            // IP形式的URL -> 199.194.52.184
+                + "|"                                                        // 允许IP和DOMAIN（域名）
+                + "(.)*"                                                     // 域名 -> www.
+                // + "([0-9a-z_!~*'()-]+\\.)*"                               // 域名 -> www.
+                + "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\\."                    // 二级域名
+                + "[a-z]{2,6})"                                              // first level domain -> .com or .museum
+                + "(:[0-9]{1,4})?"                                           // 端口 -> :80
+                + "((/?)|"                                                   // a slash isn't required if there is no file name
+                + "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";
 
         Pattern pat = Pattern.compile(regex.trim());//对比
         Matcher mat = pat.matcher(s.trim());
